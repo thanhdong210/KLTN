5f603754e3bc7f3ecebafdd8de4315ed3863edd6
unknown <nnguynthanh@gmail.com>
2022-10-28 22:12:39 +0700

    adding more function

--- models/hr_timesheet_line.py
+++ models/hr_timesheet_line.py
@@ -7,10 +7,12 @@ class HrContractTypeInherit(models.Model):
     employee_id = fields.Many2one("hr.employee", string="Employee")
     date = fields.Date(string="Date")
     timesheet_id = fields.Many2one("hr.timesheet", string="Timesheet", ondelete='cascade')
-    number_of_hour = fields.Float(string="Number of hour")
-    number_of_day = fields.Float(string="Number of day")
+    number_of_hours = fields.Float(string="Number of hour")
+    number_of_days = fields.Float(string="Number of day")
     timesheet_type_id = fields.Many2one('hr.timesheet.type', string="Timesheet Type")
     code = fields.Char(string="Code")
+    hour_from = fields.Datetime(string="Hour from")
+    hour_to = fields.Datetime(string="Hour to")
     
 
 
--- models/resource_calendar.py
+++ models/resource_calendar.py
@@ -3,7 +3,14 @@ from odoo import fields, models, _
 class ResourceCalendarInherit(models.Model):
     _inherit = "resource.calendar"
     
-    
+    mode = fields.Selection([
+        ('begin_end_time', 'By late or soon'),
+        ('worked_hours', 'By worked hours'),
+    ], string='Mode', store=True, tracking=True, copy=False, default="begin_end_time")
+    hour_late = fields.Float(string="Hour late")
+    hour_soon = fields.Float(string="Hour soon")
+    hour_to_have_one_work_day = fields.Float(string="Hour to have one work day")
+    hour_to_have_half_work_day = fields.Float(string="Hour to have half work day")
     
 
 
